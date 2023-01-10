8d84d1539ff71420ac8acb89eb20287f3b496c3d
Ivan <ivanoid768@gmail.com>
2023-01-10 18:52:57 +0600

    User Model.

---
+++ src/models/User.ts
@@ -0,0 +1,47 @@
+import {
+    getModelForClass,
+    pre,
+    prop,
+    DocumentType,
+} from "@typegoose/typegoose";
+import { hash, compare } from "bcrypt";
+
+@pre<UserClass>("save", async function () {
+    let hashed_password = await hash(this.password, 10);
+
+    this.password = hashed_password;
+})
+class UserClass {
+    @prop({
+        required: true,
+    })
+    public name!: string;
+
+    @prop({
+        required: true,
+    })
+    public lastname!: string;
+
+    @prop()
+    public patronymic?: string;
+
+    @prop({
+        unique: true,
+        required: true,
+    })
+    public phone!: number;
+
+    @prop({
+        required: true,
+    })
+    public password!: string;
+
+    public async check_passwrord(
+        this: DocumentType<UserClass>,
+        password: string
+    ) {
+        return compare(password, this.password);
+    }
+}
+
+export const UserModel = getModelForClass(UserClass);
